47ffb1bb8f5660719cd679cba0f66ec01852c301
Recep Öztürk <52690736+recepozturk53@users.noreply.github.com>
2024-05-14 13:01:57 +0300

    extend behind app bar and safe area adde

--- lib/ui/screens/test.dart
+++ lib/ui/screens/test.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 class ExerciseListTest extends StatelessWidget {
   final Random _random = Random();
 
+  ExerciseListTest({super.key});
+
   Color _generateRandomColor() {
     return Color.fromARGB(
       255,
@@ -18,17 +20,21 @@ class ExerciseListTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Playlist Name', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.transparent,
+        title:
+            const Text('Playlist Name', style: TextStyle(color: Colors.white)),
         actions: [
           IconButton(
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
             onPressed: () {
               // Implement search functionality
             },
           ),
           IconButton(
-            icon: Icon(Icons.more_vert),
+            icon: const Icon(Icons.more_vert),
             onPressed: () {
               // Show more options
             },
@@ -53,66 +59,68 @@ class ExerciseListTest extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: AspectRatio(
-                  aspectRatio: 1.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image.network(
-                      'https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80',
-                      fit: BoxFit.cover,
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        'https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80',
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  'Playlist Name',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Playlist Name',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 10, // Assuming 10 tracks in the playlist
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage('assets/album_cover.jpg'),
-                      ),
-                      title: Text(
-                        'Track Name $index',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Artist Name',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      trailing: Icon(Icons.play_circle_filled,
-                          color: Colors.white), // Playback control
-                      onTap: () {
-                        // Play the selected track
-                      },
-                    );
-                  },
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 10, // Assuming 10 tracks in the playlist
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        leading: const CircleAvatar(
+                          backgroundImage: AssetImage('assets/album_cover.jpg'),
+                        ),
+                        title: Text(
+                          'Track Name $index',
+                          style: const TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: const Text(
+                          'Artist Name',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        trailing: const Icon(Icons.play_circle_filled,
+                            color: Colors.white), // Playback control
+                        onTap: () {
+                          // Play the selected track
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0, // Assuming the playlist tab is selected
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
